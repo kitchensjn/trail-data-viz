fbda1038512073abf7933dbe8cf449bffe3da792
James Kitchens <40303683+kitchensjn@users.noreply.github.com>
2021-03-14 17:19:21 -0700

    Update postholer-scraper.py

--- data/scraper-scripts/postholer-scraper.py
+++ data/scraper-scripts/postholer-scraper.py
@@ -1,3 +1,11 @@
+"""
+title: postholer-scraper.py
+author: James Kitchens
+date: March 14, 2021
+description:
+    This script scrapes Appalachian Trail waypoint information from the Postholer website and saves it as a CSV.
+"""
+
 import requests
 from bs4 import BeautifulSoup
 
@@ -13,10 +21,37 @@ def table_to_array(table):
     return array
 
 
-sections = [0.0, 53.3, 166.4, 276.5, 393.9, 467.6, 587.9, 703.1, 784.7, 862.0, 979.9, 1026.1, 1124.5, 1223.5, 1299.5, 1408.3, 1503.2, 1613.4, 1706.1, 1750.1, 1856.9, 1963.8, 2064.6]
+# Postholer has broken the AT into segments defined by starting mileage
+# These starting mileages are used for the URLs 
+sections = [
+    0.0,
+    53.3,
+    166.4,
+    276.5,
+    393.9,
+    467.6,
+    587.9,
+    703.1,
+    784.7,
+    862.0,
+    979.9,
+    1026.1,
+    1124.5,
+    1223.5,
+    1299.5,
+    1408.3,
+    1503.2,
+    1613.4,
+    1706.1,
+    1750.1,
+    1856.9,
+    1963.8,
+    2064.6
+]
 
+# Loops through the sections and scrapes waypoints table
+# Aggregates to all_data array
 all_data = []
-
 header = True
 for sec in sections:
     section_data = BeautifulSoup(requests.get("https://www.postholer.com/databook/Appalachian-Trail/3/" + str(sec)).text, "html.parser")
@@ -28,6 +63,9 @@ for sec in sections:
     for row in array[2:]:
         all_data.append(row)
 
+
+# Save select waypoint's description, mile, and elevation to CSV
+# Removes waypoints without mileage and elevation data
 with open('../AT-waypoints.csv', 'w') as outfile:
     number_of_rows = len(all_data)
     for row in range(number_of_rows):
@@ -40,16 +78,3 @@ with open('../AT-waypoints.csv', 'w') as outfile:
                     outfile.write(all_data[row][column])
             if row != number_of_rows - 1:
                 outfile.write("\n")
-
-"""
-with open('output.csv', 'w') as waypoints_outfile:
-    waypoints = postholer_data.find("table", {"class": "wptTable"})
-    for row in waypoints.findAll('tr'):
-        columns = row.findAll('td')
-        output_row = []
-        for column in columns:
-            waypoints_outfile.write(column.text + ",")
-            #output_row.append(column.text)
-        waypoints_outfile.write("\n")
-        #output_rows.append(output_row)
-"""
